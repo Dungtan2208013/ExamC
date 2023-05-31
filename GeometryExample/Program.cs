7222d813aa93f802f300e1159d2ae076e206cf8e
DungTan <dungptth2208013@fpt.edu.vn>
2023-05-31 09:45:43 +0700

    .

---
+++ GeometryExample/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace GeometryExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cylinder cylinder = new Cylinder();
+            Console.WriteLine("Cylinder Characteristics:");
+            Console.Write(cylinder);
+            cylinder.Process();
+            cylinder.Result();
+        }
+    }
+}
---
+++ Measurement/Program.cs
@@ -0,0 +1,17 @@
+﻿using Animal;
+
+namespace Measurement
+{
+     class Program
+     {
+        static void Main(string[] args)
+        {
+            Lion lion = new Lion(200, "lion");
+            Tiger tiger = new Tiger(100, "tiger");
+
+            lion.Show();
+            tiger.Show();
+        }
+     }
+
+}
